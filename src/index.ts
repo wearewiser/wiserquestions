3c1af122590b8eee995454c5977a067f227ae40f
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:33:02 -0500

    Setup exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello, world');
+export * from './five-point-slider-wiser-question';
+export * from './multiple-choice-wiser-question';
+export * from './wiser-question-loader';
+export * from './wiser-question-types';
+export * from './wiser-question';
+export * from './wiser-questionnaire';
