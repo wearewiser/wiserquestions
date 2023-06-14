169b2aa495a4dab6e704a7819a944ad9797757bb
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:16:40 -0500

    Exporting models

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export * from './normalized-wiser-question-answer';
+export * from './normalized-wiser-question-answer-score';
+export * from './normalized-wiser-question-form';
+export * from './normalized-wiser-question-information';
+export * from './normalized-wiser-question-score';
