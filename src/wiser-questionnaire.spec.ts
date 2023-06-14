e5b05adaf77ff07ca91a17eb68fa8a364c8c514c
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:20:33 -0500

    Added empty spec for WiserQuestionnaire

---
+++ src/wiser-questionnaire.spec.ts
@@ -0,0 +1,26 @@
+describe('WiserQuestionnaire', () => {
+  describe("init()", () => {
+    it("should...");
+  });
+  describe("readWiserQuestionAtIndex()", () => {
+    it("should...");
+  });
+  describe("readCurrentWiserQuestion()", () => {
+    it("should...");
+  });
+  describe("readNextWiserQuestion()", () => {
+    it("should...");
+  });
+  describe("readPrevWiserQuestion()", () => {
+    it("should...");
+  });
+  describe("readSize()", () => {
+    it("should...");
+  });
+  describe("readIndex()", () => {
+    it("should...");
+  });
+  describe("toString()", () => {
+    it("should...");
+  });
+});
