64a6dbedaf55107832bf384ef866746a69a31eb8
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:16:09 -0500

    Added model NormalizedWiserQuestionAnswerScore

---
+++ src/models/normalized-wiser-question-answer-score.ts
@@ -0,0 +1,6 @@
+import { NormalizedWiserQuestionAnswer } from "./normalized-wiser-question-answer";
+import { NormalizedWiserQuestionScore } from "./normalized-wiser-question-score";
+
+export interface NormalizedWiserQuestionAnswerScore extends NormalizedWiserQuestionAnswer, NormalizedWiserQuestionScore {
+
+}
