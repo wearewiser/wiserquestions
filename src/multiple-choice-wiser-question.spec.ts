1fb4ff536ee72e2143b5e2bad6a19d78ea2325a1
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:18:41 -0500

    Added empty spec for MultipleChoiceWiserQuestion

---
+++ src/multiple-choice-wiser-question.spec.ts
@@ -0,0 +1,11 @@
+describe('MultipleChoiceWiserQuestion', () => {
+  describe("selectAnswer()", () => {
+    it("should...");
+  });
+  describe("setAnswerLabels()", () => {
+    it("should...");
+  });
+  describe("toString()", () => {
+    it("should...");
+  });
+});
