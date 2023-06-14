0c0cd8ea5b523f0794db97aa18bb013efc12f972
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-14 15:19:54 -0500

    Added empty spec for WiserQuestionFactory

---
+++ src/wiser-question-factory.spec.ts
@@ -0,0 +1,11 @@
+describe('WiserQuestion', () => {
+  describe("getInstance()", () => {
+    it("should...");
+  });
+  describe("returnWiserQuestionText()", () => {
+    it("should...");
+  });
+  describe("setAnswerScore()", () => {
+    it("should...");
+  });
+});
