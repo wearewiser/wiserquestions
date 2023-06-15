6be4ac0c54d44a079fd3f2e0955ee68dff36b9a8
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:32:31 -0500

    Added spec for WiserQuestionFactory

--- src/wiser-question-factory.spec.ts
+++ src/wiser-question-factory.spec.ts
@@ -1,5 +1,16 @@
+import { expect } from "chai";
+import { WiserQuestionFactory } from "./wiser-question-factory";
+import { WiserQuestionTypes } from "./wiser-question-types";
+import { MultipleChoiceWiserQuestion } from "./multiple-choice-wiser-question";
+import { FivePointSliderWiserQuestion } from "./five-point-slider-wiser-question";
+
 describe('WiserQuestionFactory', () => {
   describe("getInstance()", () => {
-    it("should...");
+    it('should return instanceof MultipleChoiceWiserQuestion when row type matches WiserQuestionTypes.MULTIPLE_CHOICE', () => {
+      expect(WiserQuestionFactory.getInstance({ Type: WiserQuestionTypes.MULTIPLE_CHOICE } as any)).to.be.instanceOf(MultipleChoiceWiserQuestion);
+    });
+    it('should return instanceof FivePointSliderWiserQuestion when row type matches WiserQuestionTypes.MULTIPLE_CHOICE', () => {
+      expect(WiserQuestionFactory.getInstance({ Type: WiserQuestionTypes.FIVE_POINT_SLIDER } as any)).to.be.instanceOf(FivePointSliderWiserQuestion);
+    });
   });
 });
