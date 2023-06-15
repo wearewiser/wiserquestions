0abbfae845d64cf87dad57e52bddde1185b2700c
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:32:46 -0500

    Implemented WiserQuestionFactory to pass unit tests

--- src/wiser-question-factory.ts
+++ src/wiser-question-factory.ts
@@ -1,8 +1,18 @@
+import { FivePointSliderWiserQuestion } from "./five-point-slider-wiser-question";
 import { NormalizedWiserQuestionForm } from "./models";
+import { MultipleChoiceWiserQuestion } from "./multiple-choice-wiser-question";
 import { WiserQuestion } from "./wiser-question";
+import { WiserQuestionTypes } from "./wiser-question-types";
 
 export class WiserQuestionFactory {
-  public static getInstance(_row: NormalizedWiserQuestionForm): WiserQuestion | never {
-    throw new Error("Method not implemented");
+  public static getInstance(row: NormalizedWiserQuestionForm): WiserQuestion | never {
+    switch (row.Type) {
+      case WiserQuestionTypes.MULTIPLE_CHOICE:
+        return new MultipleChoiceWiserQuestion(row.WiserQuestion);
+      case WiserQuestionTypes.FIVE_POINT_SLIDER:
+        return new FivePointSliderWiserQuestion(row.WiserQuestion);
+      default:
+        throw new Error(`Unknown question type ${row.Type}`);
+    }
   }
 }
