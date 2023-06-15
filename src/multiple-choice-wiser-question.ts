2bac0359522f415cd9bc00d710ff0a8ec116878d
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:32:10 -0500

    Implemented MultipleChoiceWiserQuestion to pass unit tests

--- src/multiple-choice-wiser-question.ts
+++ src/multiple-choice-wiser-question.ts
@@ -1,20 +1,43 @@
+import { EOL } from "os";
 import { NormalizedWiserQuestionAnswer } from "./models";
 import { WiserQuestion } from "./wiser-question";
 
 export class MultipleChoiceWiserQuestion extends WiserQuestion {
-  public selectAnswer(_answer: number | string): void {
-    throw new Error("Method not implemented");
+  protected answer: string;
+  protected answer_scores: Map<string, Map<string, number>>;
+  constructor(question_text: string) {
+    super(question_text);
+    this.answer = "";
+    this.answer_scores = new Map<string, Map<string, number>>();
   }
-  public setAnswerLabels(_row: NormalizedWiserQuestionAnswer): void {
-    throw new Error("Method not implemented");
+  public selectAnswer(answer: number | string): void {
+    this.testAnswerOrFail(answer as string);
+    this.answer = answer as string;
+  }
+  public setAnswerLabels(row: NormalizedWiserQuestionAnswer): void {
+    this.answer_labels.push(row["Answer Labels"]);
   }
   public toString(): string {
-    throw new Error("Method not implemented");
+    let str = super.toString();
+    str += EOL;
+    str += `   ${this.answer_labels.join(', ')}${EOL}`;
+    this.answer_scores.forEach((scores, key) => {
+      str += `   [${key.toString()}]:${EOL}`;
+      scores.forEach((v, k) => {
+        str += `      ${k.slice(0, 36).padEnd(36, "-")}: ${v}${EOL}`
+      })
+    })
+    return str;
   }
-  protected registerAnswerScoreWithAnswer(_row: NormalizedWiserQuestionAnswer, _score: Map<string, number>): void {
-    throw new Error("Method not implemented");
+  protected registerAnswerScoreWithAnswer(row: NormalizedWiserQuestionAnswer, score: Map<string, number>): void {
+    const answer: string = row.Answers;
+    this.testAnswerOrFail(answer);
+    this.answer_scores.set(answer, score);
   }
-  protected testAnswerOrFail(_answer: string): void | never {
-    throw new Error("Method not implemented");
+  protected testAnswerOrFail(answer: string): void | never {
+    if (typeof answer !== "string") {
+      throw new Error(`Selected answer must be in options. Question "${this.question_text}" received answer ${answer}`);
+    }
+    this.answer = answer;
   }
 }
