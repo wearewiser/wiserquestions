d6f87b2a9ad359c2db0bc757bbe701a8ee2b7b20
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:41:16 -0500

    Added spec for WiserQuestionTypes

--- src/wiser-question-types.spec.ts
+++ src/wiser-question-types.spec.ts
@@ -1,4 +1,11 @@
+import { expect } from "chai";
+import { WiserQuestionTypes } from "./wiser-question-types";
+
 describe('WiserQuestionTypes', () => {
-  it('should...', async () => {
+  it('should return "Multiple choice" for MULTIPLE_CHOICE', () => {
+    expect(WiserQuestionTypes.MULTIPLE_CHOICE).to.equal("Multiple choice");
+  });
+  it('should return "5-points slider" for FIVE_POINT_SLIDER', () => {
+    expect(WiserQuestionTypes.FIVE_POINT_SLIDER).to.equal("5-points slider");
   });
 });
