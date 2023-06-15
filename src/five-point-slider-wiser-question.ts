877a74ee255d8bd240a615319f22b8f1220b5aea
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:29:21 -0500

    Implemented FivePointSliderWiserQuestion to pass unit tests

--- src/five-point-slider-wiser-question.ts
+++ src/five-point-slider-wiser-question.ts
@@ -1,20 +1,47 @@
+import { EOL } from "os";
 import { NormalizedWiserQuestionAnswer } from "./models";
 import { WiserQuestion } from "./wiser-question";
 
 export class FivePointSliderWiserQuestion extends WiserQuestion {
-  public selectAnswer(_answer: number | string): void {
-    throw new Error("Method not implemented");
+  protected answer: number;
+  protected answer_scores: Map<number, Map<string, number>>;
+  constructor(question_text: string) {
+    super(question_text);
+    this.answer = -1;
+    this.answer_scores = new Map<number, Map<string, number>>();
   }
-  public setAnswerLabels(_row: NormalizedWiserQuestionAnswer): void {
-    throw new Error("Method not implemented");
+  public selectAnswer(answer: number | string): void {
+    this.testAnswerOrFail(answer as number);
+    this.answer = answer as number;
+  }
+  public setAnswerLabels(row: NormalizedWiserQuestionAnswer): void {
+    if (row.Answers === "1") {
+      this.answer_labels[0] = row["Answer Labels"];
+    }
+    if (row.Answers === "5") {
+      this.answer_labels[1] = row["Answer Labels"];
+    }
   }
   public toString(): string {
-    throw new Error("Method not implemented");
+    let str = super.toString();
+    str += EOL;
+    str += `   (1) ${this.answer_labels[0]} ... (5) ${this.answer_labels[1]}${EOL}`;
+    this.answer_scores.forEach((scores, key) => {
+      str += `  [${key.toString().padStart(2, " ")}]:${EOL}`;
+      scores.forEach((v, k) => {
+        str += `      ${k.slice(0, 36).padEnd(36, "-")}: ${v}${EOL}`
+      })
+    })
+    return str;
   }
-  protected registerAnswerScoreWithAnswer(_row: NormalizedWiserQuestionAnswer, _score: Map<string, number>): void {
-    throw new Error("Method not implemented");
+  protected registerAnswerScoreWithAnswer(row: NormalizedWiserQuestionAnswer, score: Map<string, number>): void {
+    const answer = Number(row.Answers);
+    this.testAnswerOrFail(answer);
+    this.answer_scores.set(answer, score);
   }
-  protected testAnswerOrFail(_answer: number): void | never {
-    throw new Error("Method not implemented");
+  protected testAnswerOrFail(answer: number): void | never {
+    if (typeof answer !== "number" || !Number.isInteger(answer) || answer < 1 || answer > 5) {
+      throw new Error(`Answer must be positive integer from 1-5. Question "${this.question_text}" received answer ${answer}`);
+    }
   }
 }
