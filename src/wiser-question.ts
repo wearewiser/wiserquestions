eef1d63e7e64364cb9aa77c7eca440a9d9295f5b
John Fedoruk <johnny@johnfedoruk.ca>
2023-06-15 11:28:11 -0500

    Implemented WiserQuestion to pass unit tests

--- src/wiser-question.ts
+++ src/wiser-question.ts
@@ -1,15 +1,31 @@
-import { NormalizedWiserQuestionAnswer, NormalizedWiserQuestionAnswerScore } from "./models";
+import { NormalizedWiserQuestionAnswer, NormalizedWiserQuestionAnswerScore, NormalizedWiserQuestionScore } from "./models";
 
 export abstract class WiserQuestion {
-  constructor(_question_text: string) {}
+  protected question_text: string;
+  protected answer_labels: [string, string] | string[];
+  constructor(question_text: string) {
+    this.question_text = question_text;
+    this.answer_labels = [];
+  }
   public returnWiserQuestionText(): string {
-    throw new Error("Method not implemented");
+    return this.question_text;
   }
   public toString(): string {
-    throw new Error("Method not implemented");
+    let str = "";
+    str += this.question_text;
+    return str;
   }
-  public setAnswerScore(_row: NormalizedWiserQuestionAnswerScore): void {
-    throw new Error("Method not implemented");
+  public setAnswerScore(row: NormalizedWiserQuestionAnswerScore): void {
+    const score_labels: NormalizedWiserQuestionScore = Object.assign({}, row);
+    [
+      'Question',
+      'Type',
+      'Answer Labels',
+      'Answers',
+    ].forEach(x => delete score_labels[x]);
+    const scores = new Map<string, number>();
+    Object.keys(score_labels).forEach(label => scores.set(label, Number(row[label])));
+    this.registerAnswerScoreWithAnswer(row, scores);
   }
   public abstract selectAnswer(answer: number): void;
   public abstract selectAnswer(answer: string): void;
